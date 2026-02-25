1b7effd0cd1b2bf529d1233aadd70c7ea8346ed2
Joe Sjoberg <aSquidsBody@gmail.com>
2026-02-25 08:01:23 -0600

    adding formatting keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,8 @@
-vim.g.mapleader=" "
+vim.g.mapleader = " "
 
 -- yank
 vim.keymap.set("v", "<Leader>y", '"+y')
-vim.keymap.set({"n", "v" }, "<Leader>Y", '"+Y')
+vim.keymap.set({ "n", "v" }, "<Leader>Y", '"+Y')
 
 -- remove matches when clearing search
 vim.keymap.set("n", "<ESC>", ":noh<CR>", { noremap = true, silent = true })
@@ -15,13 +15,18 @@ vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 
 -- format file
-vim.keymap.set("n", "<Leader>f", "gg=G")
-vim.keymap.set("v", "<Leader>f", "=")
+vim.keymap.set({ "n", "v" }, "<Leader><Leader>f", function()
+  vim.lsp.buf.format()
+end, { desc = "Format the selected region, or the entire buffer" })
 
 -- new window
 vim.keymap.set("n", "<Leader>pn", "<C-w>s")
 vim.keymap.set("n", "<Leader>pm", "<C-w>v")
 
 -- window resize
-vim.keymap.set('n', '<C-=>', function() vim.cmd("resize +8") end, { noremap = false, silent = true})
-vim.keymap.set('n', '<C-->', function() vim.cmd("resize -8") end, { noremap = true, silent = true})
+vim.keymap.set("n", "<C-=>", function()
+  vim.cmd("resize +8")
+end, { noremap = false, silent = true })
+vim.keymap.set("n", "<C-->", function()
+  vim.cmd("resize -8")
+end, { noremap = true, silent = true })
