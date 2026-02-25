913bfb3b2b67a53ecf5964141822b67f4bbf66ab
Joseph Sjoberg <jsjoberg@macbook.local>
2026-02-25 17:56:27 -0600

    update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,41 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = 'v0.1.9',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  "nvim-telescope/telescope.nvim",
+  tag = "v0.1.9",
+  dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
     {
       "<Leader>pf",
       function()
         local builtin = require("telescope.builtin")
-        builtin.find_files()
+        builtin.find_files(require("telescope.themes").get_dropdown({ previewer = false }))
       end,
       mode = { "n" },
-      desc = "Find files"
+      desc = "Find files",
+    },
+    {
+      "<Leader>pn",
+      function()
+        local builtin = require("telescope.builtin")
+        builtin.find_files({
+          cwd = vim.fn.stdpath("config"),
+          border = true,
+          borderchars = {
+            preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+            prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
+            results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
+          },
+          layout_config = {
+            preview_cutoff = 1,
+          },
+          layout_strategy = "center",
+          previewer = false,
+          results_title = false,
+          sorting_strategy = "ascending",
+          theme = "dropdown",
+        })
+      end,
+      mode = { "n" },
+      desc = "Find files in nvim directory",
     },
     {
       "<Leader>pg",
@@ -19,7 +44,7 @@ return {
         builtin.live_grep()
       end,
       mode = { "n" },
-      desc = "Grep files"
+      desc = "Grep files",
     },
     {
       "<Leader>pg",
@@ -28,7 +53,7 @@ return {
         builtin.grep_string()
       end,
       mode = { "v" },
-      desc = "Grep string under cursor"
-    }
-  }
+      desc = "Grep string under cursor",
+    },
+  },
 }
