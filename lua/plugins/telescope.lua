9d04d4d2037a54dcd0bc26af6e69d37d83753a28
Joe Sjoberg <aSquidsBody@gmail.com>
2026-02-26 13:45:30 -0600

    adding better find files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,10 +7,34 @@ return {
       "<Leader>pf",
       function()
         local builtin = require("telescope.builtin")
-        builtin.find_files()
+        builtin.find_files(require("telescope.themes").get_dropdown({ previewer = false }))
       end,
       mode = { "n" },
       desc = "Find files"
+    },
+    {
+      "<Leader>nf",
+      function()
+        local builtin = require("telescope.builtin")
+        local opts = require("telescope.themes").get_dropdown({ previewer = false })
+        opts.cwd = vim.fn.stdpath("config")
+        opts.prompt_title = "Neovim Config - Find Files"
+        builtin.find_files(opts)
+      end,
+      mode = { "n" },
+      desc = "Find files in neovim config"
+    },
+    {
+      "<Leader>df",
+      function()
+        local builtin = require("telescope.builtin")
+        local opts = require("telescope.themes").get_dropdown({ previewer = false })
+        opts.cwd = "/Users/jsjoberg/java/dataproc-api"
+        opts.prompt_title = "Dataproc API - Find Files"
+        builtin.find_files(opts)
+      end,
+      mode = { "n" },
+      desc = "Find files in neovim config"
     },
     {
       "<Leader>pg",
