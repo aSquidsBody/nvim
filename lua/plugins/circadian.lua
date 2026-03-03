dba685d23071dcfe9cb06387dc42dd5acb2720d7
Joe Sjoberg <aSquidsBody@gmail.com>
2026-03-03 13:42:30 -0600

    adding local changes

--- lua/plugins/circadian.lua
+++ lua/plugins/circadian.lua
@@ -4,6 +4,6 @@ return {
     lat = 30.2672,
     lon = -97.7431,
     day = { background = "dark", colorscheme = "cobalt" },
-    night = { background = "dark", colorscheme = "jellybeans" },
+    night = { background = "dark", colorscheme = "cobalt" },
   }
 }
